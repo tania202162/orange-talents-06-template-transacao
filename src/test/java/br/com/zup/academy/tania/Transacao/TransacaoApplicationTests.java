6763e671b1bd861417f408db589a9745ea986e33
unknown <tania.alves@zup.com.br>
2021-08-13 09:58:03 -0300

    Setup-Transacao

---
+++ src/test/java/br/com/zup/academy/tania/Transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.tania.Transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
